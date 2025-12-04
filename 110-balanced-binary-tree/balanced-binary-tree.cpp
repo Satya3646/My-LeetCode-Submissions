8db5501e4455b72fa82b5f618ae96027733a3fe2
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-04 16:25:26 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (22.92%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -15,18 +15,16 @@ public:
     {
         if(!root)
             return 0;
-        int left = height(root->left);
-        if(left == -1)
-            return -1;  
-        int right = height(root->right);
-        if(right == -1)
+        int left = height(root->left); // Get height of left subtree
+        int right = height(root->right); // Get the height of the right subtree
+        if(left == -1 || right == -1) // If either is unbalanced then return -1 as a failure signal
             return -1;
-        if(abs(left - right) > 1)
+        if(abs(left - right) > 1) // If the current root is unbalanced the return -1 as a failure signal
             return -1;
-        return 1 + max(left, right);
+        return 1 + max(left, right); // If all above cases fail then return the height till current node from the bottom
     }
 
     bool isBalanced(TreeNode* root) {
-        return height(root) != -1;
+        return height(root) != -1; // Get the height of the tree, if -1 is returned then balance failed at some node so return false else return true
     }
 };
