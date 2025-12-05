80ced64346967fc3fdd754a0e168bcbb75936d56
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-05 22:06:35 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (96.78%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int countPartitions(vector<int>& nums) {
         int n = nums.size();
-        int rSum = accumulate(nums.begin(), nums.end(), 0);
+        int rSum = accumulate(nums.begin(), nums.end(), 0); // Sum or right partition
+        int lSum = 0; // Sum of left partition
         int ans = 0;
-        int lSum = 0;
-        for(int i = 0; i < n-1; i++)
+        // Loop from 0 to n-1, the whole array cant be left partition as per the question.
+        // According to the question a partition can only be made between any two elements and not at end of the array.
+        for(int i = 0; i < n-1; i++) 
         {
-            lSum += nums[i];
-            rSum -= nums[i];
-            if(abs(lSum - rSum) & 1)
+            lSum += nums[i]; // Update lsum
+            rSum -= nums[i]; // Update rsum
+            if(abs(lSum - rSum) & 1) // If their difference is odd then skip the answer update
                 continue;
-            ans++;
+            ans++; // Answer will get updated if the difference is even
         }
         return ans;
     }
