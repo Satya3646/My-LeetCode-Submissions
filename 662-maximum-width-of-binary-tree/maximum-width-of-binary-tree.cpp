e2b9325efdac8884b8407d68afac7fb2dc4cf13d
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-09 10:54:06 +0530

    Time: 3 ms (14.02%) | Memory: 19.7 MB (41.03%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -12,13 +12,27 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        queue <pair <TreeNode*, unsigned long long>> q;
+        /*
+        To solve this problem we need the distance between the leftmost non null node to the 
+        rightmost non null node of the tree, but during the count we need to even consider the null 
+        nodes for the count. 
+        Treat each level of the tree as an array. For root the index is 0, for next level the indice
+        are 0(left) and 1(right).
+                     0 (root)
+                   /   \
+                  0     1
+                 / \   / \
+                0   1 2   3
+        If we the observe the trend the indexing is following the rule as if x is the index of  
+        parent the left child has the index 2x and right child has the index 2x+1.
+        */
+        queue <pair <TreeNode*, unsigned long long>> q; // Store the node along with its respective index.
         q.push({root, 0});
         unsigned long long ans = 0;
         while(!q.empty())
         {
             int n = q.size();
-            unsigned long long l = 0, r = 0;
+            unsigned long long l = 0, r = 0; // Capture index of leftmost non null node and rightmost non null node.
             for(int i = 0; i < n; i++)
             {
                 TreeNode* node = q.front().first;
@@ -33,7 +47,7 @@ public:
                 if(node->right)
                     q.push({node->right, (x<<1)+1});
             }
-            ans = max(ans, r-l+1);
+            ans = max(ans, r-l+1); // Update the ans.
         }
         return ans;
     }
