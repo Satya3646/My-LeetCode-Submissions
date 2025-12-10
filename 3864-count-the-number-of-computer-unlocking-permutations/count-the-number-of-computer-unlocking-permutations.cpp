c716224e9c4c4c0ed938c1b86462f73d7e74a9d4
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-10 10:19:06 +0530

    Time: 0 ms (100.00%) | Memory: 92.7 MB (71.00%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int countPermutations(vector<int>& complexity) {
         int n = complexity.size();
+        // Computer 0 can act as key for all the computers.
         int key = complexity[0];
-        for(int i = 1; i < n; i++)
-            if(complexity[i] <= key)
+        for(int i = 1; i < n; i++) // If a computer with same or less complexity exists then zero 
+            if(complexity[i] <= key) // cant unlock hence no order exists.
                 return 0;
+        // if zero can unlock all the computers then ans is arrangement of n-1 computers in different ways.
         int mod = 1e9+7;
         int k = n-1;
         long long ans = 1;
