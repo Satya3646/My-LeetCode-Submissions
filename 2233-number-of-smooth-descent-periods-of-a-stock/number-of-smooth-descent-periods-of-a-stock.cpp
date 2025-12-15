b87c5d7a58b78e6cd13a0032cee392a947a0dcbc
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-15 10:43:59 +0530

    Time: 0 ms (100.00%) | Memory: 106.8 MB (97.03%) - LeetSync

--- 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.cpp
@@ -5,15 +5,15 @@ public:
         int n = prices.size();
         long long ans = 0;
         int cnt = 0;
-        int i = 0; 
+        int i = 0;
         while(i < n)
         {
-            int j = i+1;
-            while(j < n && prices[j-1] - prices[j] == 1)
+            int j = i+1; // Right pointer to expand the window.
+            while(j < n && prices[j-1] - prices[j] == 1) // Go right as long as smooth descent is there.
                 j++;
-            int len = j-i;
+            int len = j-i; // Get the length of the segment with smooth descent.
             i = j;
-            ans += 1LL * len * (len+1) / 2;
+            ans += 1LL * len * (len+1) / 2; // Number of subarrays = (n x (n+1))/2.
         }
         return ans;
     }
