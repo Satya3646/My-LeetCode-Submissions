a23b8e6ae3d4a073fcec07747d37bdb6d3c5e59f
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-16 11:17:44 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (21.72%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,23 +11,14 @@
  */
 class Solution {
 public:
-    TreeNode* solve(vector <int> &preorder, int low, int high)
+    int indx = 0;
+    TreeNode* bstFromPreorder(vector <int> &preorder, int bound = INT_MAX)
     {
-        if(low > high) // Breaking condition.
+        if(indx == preorder.size() || preorder[indx] > bound)
             return nullptr;
-        TreeNode* root = new TreeNode(preorder[low]); // Create the root.
-        int i = low+1; // Collect the left subtree (all values less than root). 
-        while(i <= high && preorder[i] < preorder[low])
-            i++;
-        // now low is the root, low+1 to i-1 will be values less than the root (left subtree), and i to high will be the values greater than the root (right subtree).
-        root->left = solve(preorder, low+1, i-1);
-        root->right = solve(preorder, i, high);
+        TreeNode* root = new TreeNode(preorder[indx++]);
+        root->left = bstFromPreorder(preorder, root->val);
+        root->right = bstFromPreorder(preorder, bound);
         return root;
-    }
-
-    TreeNode* bstFromPreorder(vector <int> &preorder)
-    {
-        int n = preorder.size();
-        return solve(preorder, 0, n-1);
     }
 };
