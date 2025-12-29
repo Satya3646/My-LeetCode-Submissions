9ab7fa2bbdac58fc2f5ac369ede4e05e7bf6c522
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-29 11:15:24 +0530

    Time: 46 ms (85.86%) | Memory: 19.8 MB (84.55%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -2,33 +2,39 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set <string> s(wordList.begin(), wordList.end()); // put all the words into unordered_set for fast search and erase.
+
         if(s.find(endWord) == s.end())
             return 0;
 
-        queue <pair <string, int>> q;
-        q.push({beginWord, 1}); // since we need to count the total words in the path pass 1.
+        queue <string> q;
+        q.push(beginWord); // since we need to count the total words in the path pass 1.
+        int words = 1;
         while(!q.empty())
         {
-            auto [str, steps] = q.front();
-            q.pop();
-            if(str == endWord)
-                return steps;
-            for(int j = 0; j < str.size(); j++) // Selects index in the current string.
+            int qs = q.size();
+            while(qs--)
             {
-                string tmp = str; // temporary string which represents the word we need to search for next step.
-                for(char c = 'a'; c <= 'z'; c++) // 
+                string str = q.front();
+                q.pop();
+                for(int i = 0; i < str.size(); i++) // Selects index in the current string.
                 {
-                    if(c == str[j]) // Same word need not be searched again.
-                        continue;
-                    tmp[j] = c; // Replace the character at jth index.
-                    auto it = s.find(tmp);
-                    if(it != s.end()) // if modified word that has one character difference to str is found the push to queue.
+                    char tmp = str[i]; // temporary string which represents the word we need to search for next step.
+                    for(char c = 'a'; c <= 'z'; c++) // 
                     {
-                        q.push({*it, steps+1});
-                        s.erase(it); // erase from the set since word can be used only once.
+                        str[i] = c; // Replace the character at jth index.
+                        auto it = s.find(str);
+                        if(it != s.end()) // if modified word that has one character difference to str is found the push to queue.
+                        {
+                            if(*it == endWord)
+                                return words+1;
+                            q.push(*it);
+                            s.erase(it); // erase from the set since word can be used only once.
+                        }
                     }
+                    str[i] = tmp;
                 }
             }
+            words++;
         }
         return 0;
     }
