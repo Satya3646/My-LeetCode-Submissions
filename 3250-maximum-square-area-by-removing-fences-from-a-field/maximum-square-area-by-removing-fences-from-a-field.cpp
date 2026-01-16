dad846bf5e52c527e752803da01d2320038603c1
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-01-17 01:42:15 +0530

    Time: 1241 ms (54.90%) | Memory: 243 MB (92.81%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -3,27 +3,32 @@ const int mod = 1e9+7;
 class Solution {
 public:
     int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
+        // push the outer boundaries into the fences since they can also form square.
         hFences.push_back(1);
         hFences.push_back(m);
         vFences.push_back(1);
         vFences.push_back(n);
-        unordered_set <int> hGaps, vGaps;
-        hGaps.reserve(hFences.size() + 2);
-        vGaps.reserve(vFences.size() + 2);
+
+        unordered_set <int> hGaps; // hash set to store horizontal gaps b/w any two fences.
+        hGaps.reserve(hFences.size());
         for(int i : hFences)
             for(int j : hFences)
-                if(j != i)
+                if(j != i) // prevent zero.
                     hGaps.insert(abs(j - i));
-        for(int i : vFences)
-            for(int j : vFences)
-                if(j != i)
-                    vGaps.insert(abs(j - i));
+
         int maxSide = -1;
-        for(int i : hGaps)
-            if(vGaps.find(i) != vGaps.end())
-                maxSide = max(maxSide, i);
-        if(maxSide == -1) 
+        for(int i : vFences)
+        {
+            for(int j : vFences)
+            {
+                int vGap = abs(j - i); // take a vertical gap between the fence i and j.
+                if(hGaps.find(vGap) != hGaps.end()) // if same gap is found in horizontal axis too then a square can be formed so update the answer.
+                    maxSide = max(maxSide, vGap);
+            }
+        }
+        
+        if(maxSide == -1) // no square formed.
             return -1;
-        return 1LL * maxSide * maxSide % mod;
+        return 1LL * maxSide * maxSide % mod; // return area.
     }
 };
