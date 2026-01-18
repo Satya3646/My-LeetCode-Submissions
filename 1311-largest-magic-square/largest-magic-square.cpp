fa0029a730d1bf074306085da52aac790d3385a4
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-01-18 21:17:17 +0530

    Time: 141 ms (29.36%) | Memory: 12.9 MB (72.67%) - LeetSync

--- 1311-largest-magic-square/largest-magic-square.cpp
+++ 1311-largest-magic-square/largest-magic-square.cpp
@@ -6,16 +6,16 @@ public:
         int n = grid[0].size();
         int maxK = 1;
         int k = 2;
-        while(k <= min(m, n))
+        while(k <= min(m, n)) // size of magic square.
         {
-            bool foundSquare = false;
-            int top = 0, bottom = k-1;
+            bool foundSquare = false; // indicates if a magic square for current k is found or not.
+            int top = 0, bottom = k-1; // top and bottom edges.
             while(bottom < m)
             {
-                int left = 0, right = k-1;
+                int left = 0, right = k-1; // left and right edges.
                 while(right < n)
                 {
-                    bool flag = true;
+                    bool flag = true; // true if current window is a magic square.
                     int sum = 0; // sum value to check with all other dimensions.
                     for(int i = left; i <= right; i++)
                         sum += grid[top][i];
@@ -24,10 +24,8 @@ public:
                     for(int i = top+1; i <= bottom; i++)
                     {
                         int rowSum = 0;
-                        for(int j = left; j <= right; j++)
-                        {
+                        for(int j = left; j <= right; j++) 
                             rowSum += grid[i][j];
-                        }
                         if(rowSum != sum)
                             flag = false;
                     }
@@ -36,41 +34,40 @@ public:
                     for(int i = left; i <= right && flag; i++)
                     {
                         int colSum = 0;
-                        for(int j = top; j <= bottom; j++)
-                        {
-                            colSum += grid[j][i];
-                        }
+                        for(int j = top; j <= bottom; j++) 
+                            colSum += grid[j][i]; 
                         if(colSum != sum)
                             flag = false;
                     }
 
                     // check diagonals.
-                    int diagSum = 0;
+                    int d1 = 0, d2 = 0;
                     for(int i = 0; i < k && flag; i++)
-                        diagSum += grid[top + i][left + i];
-                    if(diagSum != sum)
-                        flag = false;
-                    
-                    diagSum = 0;
-                    for(int i = 0; i < k && flag; i++)
-                        diagSum += grid[top + i][right - i];
-                    if(diagSum != sum)
+                    {    
+                        d1 += grid[top + i][left + i];
+                        d2 += grid[top + i][right - i];
+                    }
+                    if(d1 != sum || d2 != sum)
                         flag = false;
 
                     if(flag) // current window is a magic square.
                     {
-                        // no need to check further go for next kxk square.
+                        // no need to check further go for next k square.
                         foundSquare = true;
                         break;
                     }
+
+                    // move the window to right.
                     right++;
                     left++;
                 }
+
+                // move the window down.
                 top++;
                 bottom++;
             }
             
-            if(foundSquare)
+            if(foundSquare) // if a sqaure has been found then update the maxK value.
                 maxK = k;
             k++;
         }
