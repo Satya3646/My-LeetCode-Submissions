c439f2ab25c6c2c109fa8e9830781311738ce95e
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-01-30 17:49:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (21.41%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int minimumDifference(vector<int>& nums, int k) {
+    int minimumDifference(vector<int>& nums, int k)
+    {
         int n = nums.size();
+
+        // The dieeference between adjacent elements will be minimized in sorted order.
         sort(nums.begin(), nums.end());
 
-        int i = 0, j = k-1;
+        int i = 0, j = k-1; // place the window.
         int minDiff = INT_MAX;
-        
+
+        // Largest of the window will be at j and the smallest at i.
         while(j < n)
-        {
-            minDiff = min(minDiff, nums[j] - nums[i]);
-            i++;
-            j++;
-        }
+            minDiff = min(minDiff, nums[j++] - nums[i++]);
         return minDiff;
     }
 };
