d1b0ee7d4f8a7e2f4b536712e00f0b3ed7f08094
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-02-20 23:42:22 +0530

    Time: 40 ms (35.79%) | Memory: 18.3 MB (84.21%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,57 @@
+class dsu {
+public:
+    vector <int> parent;
+
+    dsu(int n)
+    {
+        parent.resize(n);
+
+        for(int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int findPar(int node)
+    {
+        if(parent[node] == node)
+            return node;
+        return parent[node] = findPar(parent[node]);
+    }
+
+    void Union(int u, int v)
+    {
+        int ulpu = findPar(u);
+        int ulpv = findPar(v);
+        
+        if(ulpu == ulpv)
+            return;
+
+        // here no need any criterion to merging the groups.
+        // but using size/rank will make it faster.
+        parent[ulpv] = ulpu;
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector <vector <int>> &stones) 
+    {
+        int n = stones.size();
+
+        // consider each stone as a node.
+        // If the row index or col index of a pair of nodes match then they are connected.
+        dsu grid = dsu(n);
+        for(int i = 0; i < n; i++)
+            for(int j = i+1; j < n; j++)
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])
+                    grid.Union(i, j);
+
+        // find the number of components.
+        int cnt = 0;
+        for(int i = 0; i < n; i++)
+            if(grid.parent[i] == i)
+                cnt++;
+        
+        // from each components one stone is left and rest all can be removed.
+        return n - cnt;
+    }
+};
