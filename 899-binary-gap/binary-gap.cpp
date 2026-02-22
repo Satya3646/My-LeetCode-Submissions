44f508cef445b271214124bb282353c7e3378ed8
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-02-22 09:39:23 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (70.75%) - LeetSync

--- 899-binary-gap/binary-gap.cpp
+++ 899-binary-gap/binary-gap.cpp
@@ -3,20 +3,22 @@ public:
     int binaryGap(int n) {
         int gap = 0;
         int maxGap = 0;
-        while(n && !(n&1))
+
+        while(n && !(n&1)) // shift till the first one appears. 
             n >>= 1;
+
         while(n)
         {
-            if(n & 1)
+            if(n & 1) // if bit is 1 then gap has ended.
             {
                 maxGap = max(maxGap, gap);
                 gap = 1;
             }
-            else
+            else // if bit is zero the increment gap.
                 gap++;
             n >>= 1;
         }
-        // return maxGap? maxGap+1: 0;;
+        
         return maxGap;
     }
 };
