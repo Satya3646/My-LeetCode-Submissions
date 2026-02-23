2185fe89b0eeb2c3ba8c158fcc33b86901050da8
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-02-23 23:45:53 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (65.73%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -13,10 +13,15 @@ class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum)
     {
-        if(!root)
+        // empty tree can't give any targetSum.
+        if(!root) 
             return false;
+
+        // If current node is a leaf, then if its value is equal to targetSum, its valid path.
         if(!root->left && !root->right)
             return root->val == targetSum;
+
+        // check if left subtree or right subtree have path with sum = targetSum - root->val.
         return hasPathSum(root->left, targetSum - root->val) ||
                hasPathSum(root->right, targetSum - root->val);
     }
