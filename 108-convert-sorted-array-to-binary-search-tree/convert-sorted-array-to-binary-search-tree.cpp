2e84e9949f6b0f80bcdbbd3bc2ca7f02fe75eeeb
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-02-23 14:19:21 +0530

    Time: 3 ms (62.00%) | Memory: 22.8 MB (80.94%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -1,28 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* merge(vector <int> &nums, int left, int right)
     {
-        if(left > right)
+        if(left > right) // Base case.
             return nullptr;
+
         int mid = left + ((right - left) >> 1);
-        TreeNode* root = new TreeNode(nums[mid]);
-        root->left = merge(nums, left, mid-1);
-        root->right = merge(nums, mid+1, right);
-        return root;
+        TreeNode* root = new TreeNode(nums[mid]); // Create root node.
+        
+        root->left = merge(nums, left, mid-1); // build and attach left subtree.
+        root->right = merge(nums, mid+1, right); // build and attach right subtree.
+
+        return root; // ruturn the tree.
     }
 
     TreeNode* sortedArrayToBST(vector<int>& nums) {
+        // Think of solution based on merge sort.
+        // Middle element is root the left part will become 
+        // the left subtree and right past --> right subtree.
         return merge(nums, 0, nums.size()-1);
     }
 };
