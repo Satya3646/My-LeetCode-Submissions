33a84212c2865f89b8be89065b85987cb6ffc244
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-02-28 09:33:44 +0530

    Time: 25 ms (79.19%) | Memory: 8.1 MB (69.80%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -4,15 +4,20 @@ class Solution {
 public:
     int concatenatedBinary(int n)
     {
-        long long ans = 0;
-        int currbits = 0;
+        long long ans = 0; // use long since it has 64 bits.
+        int currbits = 0; // keeps track of the number of bits current integer has.
+
         for(int i = 1; i <= n; i++)
         {
+            // The number of bits changes only when current integer is a power of 2.
             if((i & (i-1)) == 0)
                 currbits++;
+            
+            // Left shift the answer by number of bits in current integer and OR with it.
             ans = (ans << currbits) | i;
             ans %= mod;
         }
+        
         return ans;
     }
 };
