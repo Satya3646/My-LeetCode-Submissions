4ddee77603e7c244be7e8d008b8cc39e297987a5
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-03-04 18:16:03 +0530

    Time: 11 ms (42.36%) | Memory: 14.8 MB (33.84%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -5,13 +5,14 @@ class Solution {
 public:
     bool canSwimToBottom(int water, vector <vector <int>> &grid)
     {
-        if(grid[0][0] > water)
+        // starting position is not submerged yet so cant start swimming.
+        if(grid[0][0] > water) 
             return false;
             
         int n = grid.size();
         vector <vector <bool>> vis(n, vector <bool> (n, false));
 
-        queue <pair <int, int>> q;
+        queue <pair <int, int>> q; // BFS queue.
         q.push({0, 0});
         vis[0][0] = true;
         while(!q.empty())
@@ -19,13 +20,13 @@ public:
             auto [x, y] = q.front();
             q.pop();
 
-            for(int i = 0; i < 4; i++)
+            for(int i = 0; i < 4; i++) // adjacent neighbors.
             {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if(0 <= nx && nx < n && 0 <= ny && ny < n && 
-                   !vis[nx][ny] && 
-                   grid[nx][ny] <= water)
+                if(0 <= nx && nx < n && 0 <= ny && ny < n && // valid neighbor index
+                   !vis[nx][ny] && // not visited
+                   grid[nx][ny] <= water) // submerged.
                 {
                     vis[nx][ny] = true;
                     q.push({nx, ny});
@@ -33,18 +34,23 @@ public:
             }
         }
 
+        // if bottom last cell was reachable then return true.
         return vis[n-1][n-1];
     }
     
-    int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        
+    int swimInWater(vector<vector<int>>& grid)
+    {
+        /* We can do B.S. on the answer space, since monotonicity holds, if we cant 
+        swim to bottom on current time, the all smaller time values will also not allow
+        use to swim to the bottom hence we can omit them from answer space. */ 
         int low = 0, high = 2500;
         while(low <= high)
         {
             int mid = low + ((high - low) >> 1);
+            // if we can reach bottom at current mid, the go left to minimize it.
             if(canSwimToBottom(mid, grid))
                 high = mid-1;
+            // if current mid doesnt allow us to swim to bottom go right to increase it.
             else
                 low = mid+1;
         }
