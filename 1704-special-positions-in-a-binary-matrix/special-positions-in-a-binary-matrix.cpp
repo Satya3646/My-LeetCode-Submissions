43c08d8a498bd8508c77377bb24060a2b6a94b34
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-03-04 10:03:03 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (57.25%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -5,6 +5,7 @@ public:
         int n = mat[0].size();
         int ans = 0;
 
+        // calculate the row and column sums to get number of 1's in each of them.
         vector <int> rowSum(m, 0), colSum(n, 0);
         for(int i = 0; i < m; i++)
         {
@@ -15,6 +16,8 @@ public:
             }
         }
 
+        // if a position in matrix is '1' then check if its rowsum and colsum are both 1.
+        // if both are 1 that means current position is a special 1.
         for(int i = 0; i < m; i++)
             for(int j = 0; j < n; j++)
                 if(mat[i][j] && rowSum[i] == 1 && colSum[j] == 1)
