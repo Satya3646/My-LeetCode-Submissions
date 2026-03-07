63608ff464f945b264eb39bd08daff164ff493f6
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-03-07 18:13:52 +0530

    Time: 4 ms (94.40%) | Memory: 13.3 MB (99.20%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -6,30 +6,28 @@ public:
 
         // Pattern 1 = 01010101.....
         // Pattern 2 = 10101010.....
-        int misMatchPattern1, misMatchPattern2;
-        misMatchPattern1 = misMatchPattern2 = 0;
-
-        // concatenate s with itself to obtain all possible rotation of itself as sliding window.
-        s += s;
-
+        // keeping track of mismatch for pattern 1, we can get mismatch for pattern 2 as n-mismatch.
+        int mismatch = 0;
+        
+        // Place the window from 0 to n-1.
         for(int i = 0; i < n; i++)
-        {
-            misMatchPattern1 += (s[i] != ('0' + (i & 1)));
-            misMatchPattern2 += (s[i] != ('0' + !(i & 1)));
-        }
+            mismatch += (s[i] != ('0' + (i & 1)));
 
-        int flips = min(misMatchPattern1, misMatchPattern2);
+        int flips = min(mismatch, n - mismatch);
+
+        // For strings of even length Type-1 operation has no meaning since 
+        // doing Type-1 operation wont reduce the number of filps needed.
+        if(n % 2 == 0)
+            return flips;
+
+        // For all possible rotations of string get the minimum filps needed.
         for(int i = n; i < 2*n; i++)
         {
-            if(s[i-n] != ('0' + ((i-n) & 1)))
-                misMatchPattern1--;
-            if(s[i-n] != ('0' + !((i-n) & 1)))
-                misMatchPattern2--;
-            if(s[i] != ('0' + (i & 1)))
-                misMatchPattern1++;
-            if(s[i] != ('0' + !(i & 1)))
-                misMatchPattern2++;
-            flips = min({flips, misMatchPattern1, misMatchPattern2});
+            // The same character enters and leaves the window, only the parity of index changes.
+            // The character is s[i-n], but leaves at index i-n and enters at index i.
+            mismatch -= (s[i-n] != ('0' + ((i-n) & 1)));
+            mismatch += (s[i-n] != ('0' + (i & 1)));
+            flips = min({flips, mismatch, n - mismatch});
         }
 
         return flips;
