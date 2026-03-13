bbd5ee35dcbf3d42743aa6361bafd280cf1a9828
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-03-13 22:30:23 +0530

    Time: 7 ms (85.16%) | Memory: 29.1 MB (43.87%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -4,21 +4,26 @@ public:
     {
         for(int i : workerTimes)
         {
+            // Reduce the mountain height as much as the worker can in given time(mid).
             mountainHeight -= (int)(-0.5 + sqrt(0.25 + 2.0 * (double)(mid / i)));
 
+            // Short Ckt : If at any point mountain was reduced to 0.
             if(mountainHeight <= 0)
                 return true;
         }
 
-        return mountainHeight <= 0;
+        return false;
     }
 
     long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes)
     {
+        // Upper and Lower bounds.
         long long low = 1, high = 1e18;
         while(low <= high)
         {
             long long mid = low + ((high - low) >> 1);
+
+            // If mountain can be reduced to 0 the try to find smaller time.
             if(canReduceMountain(mid, mountainHeight, workerTimes))
                 high = mid-1;
             else
