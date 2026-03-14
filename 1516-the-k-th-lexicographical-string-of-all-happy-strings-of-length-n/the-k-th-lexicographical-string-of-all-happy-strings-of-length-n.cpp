e54aedca874f0c12dde0bc006f0f90ff22889c1f
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-03-15 02:20:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (49.04%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -2,53 +2,41 @@ class Solution {
 public:
     void solve(int i, int n, int k, string &curr, vector <string> &ans)
     {
-        if(ans.size() == k)
+        if(ans.size() == k) // we can stop after generating k strings.
             return;
 
-        if(i == n)
+        if(i == n) // Base case.
         {
             ans.push_back(curr);
             return;
         }
 
-        char c = curr.back();
-        if(c != 'a')
+        char c = curr.back(); // extract the current character.
+        for(int j = 0; j < 3; j++)
         {
-            curr += 'a';
-            solve(i+1, n, k, curr, ans);
-            curr.pop_back();
-        }
-
-        if(c != 'b')
-        {
-            curr += 'b';
-            solve(i+1, n, k, curr, ans);
-            curr.pop_back();
-        }
-
-        if(c != 'c')
-        {
-            curr += 'c';
-            solve(i+1, n, k, curr, ans);
-            curr.pop_back();
+            // Recursive call on different character only.
+            if(c != (char)('a' + j))
+            {
+                curr += (char)('a' + j);
+                solve(i+1, n, k, curr, ans);
+                curr.pop_back();
+            }
         }
     }
     string getHappyString(int n, int k)
     {
         vector <string> ans;
         string curr = "";
-        curr += 'a';
-        solve(1, n, k, curr, ans);
-        curr.pop_back();
+
+        // Base function call.
+        for(int i = 0; i < 3; i++)
+        {
+            curr += (char)('a' + i);
+            solve(1, n, k, curr, ans);
+            curr.pop_back();
+        }
         
-        curr += 'b';
-        solve(1, n, k, curr, ans);
-        curr.pop_back();
-
-        curr += 'c';
-        solve(1, n, k, curr, ans);
-        curr.pop_back();
-
+        // The generated strings will automatically be in lexi-order.
         return ans.size() == k? ans.back(): "";
     }
 };
