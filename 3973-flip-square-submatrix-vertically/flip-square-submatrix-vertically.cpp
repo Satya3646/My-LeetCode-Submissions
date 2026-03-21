59d81d8d4466e43710c30951d464bba663fbd1b7
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-03-21 18:44:51 +0530

    Time: 0 ms (100.00%) | Memory: 33.9 MB (31.02%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
@@ -5,11 +5,11 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        int top = x, bottom = x + k - 1;
-        int left = y, right = y + k - 1;
+        int top = x, bottom = x + k - 1; // row boundaries of the sub-matrix.
+        int left = y, right = y + k - 1; // col boundaries of the sub-matrix.
         while(top < bottom)
         {
-            for(int i = left; i <= right; i++)
+            for(int i = left; i <= right; i++) // swap the top and bottom element to reverse.
                 swap(grid[top][i], grid[bottom][i]);
 
             top++;
