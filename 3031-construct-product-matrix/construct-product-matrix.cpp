2d3c1047b4cc84409bacb55b42c2711f510dd8b6
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-03-27 11:11:03 +0530

    Time: 35 ms (46.23%) | Memory: 131.9 MB (82.66%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -9,15 +9,15 @@ public:
 
         vector <vector <int>> ans(n, vector <int> (m, 1));
 
-        long long prefixProd = grid[0][0] % mod;
+        long long prefixProd = grid[0][0];
         for(int i = 1; i < (m * n); i++)
         {
             int r = i / m, c = i % m;
-            ans[r][c] = prefixProd;
+            ans[r][c] = (prefixProd % mod);
             prefixProd = ((prefixProd % mod) * (grid[r][c] % mod)) % mod;
         }
 
-        long long suffixProd = grid[n-1][m-1] % mod;
+        long long suffixProd = grid[n-1][m-1];
         for(int i = (m * n) - 2; i >= 0; i--)
         {
             int r = i / m, c = i % m;
