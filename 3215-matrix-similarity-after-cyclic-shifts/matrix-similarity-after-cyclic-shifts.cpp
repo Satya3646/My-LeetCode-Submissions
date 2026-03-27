11608ec9bf978898329ef14c4bde295f06b15236
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-03-27 09:56:37 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (91.30%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -6,12 +6,14 @@ public:
         int n = mat[0].size();
 
         k %= n;
-        int shift;
+        int shift; // indicates direction of shift.
         for(int i = 0; i < m; i++)
         {
+            // indicates +1 (right shift) when row is odd & -1 (left shift) when row is even.
             shift = (i & 1)? 1: -1;
             for(int j = 0; j < n; j++)
             {
+                // Add +n to index to ensure +ve mod (left shift case).
                 if(mat[i][j] != mat[i][(j + k * shift + n) % n])
                     return false;
             }
