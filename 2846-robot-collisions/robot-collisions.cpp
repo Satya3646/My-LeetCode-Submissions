55f2b4aeafe67c8bf11468fb044e8f2d79709e06
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-04-01 14:22:54 +0530

    Time: 51 ms (80.84%) | Memory: 194.7 MB (85.23%) - LeetSync

--- 2846-robot-collisions/robot-collisions.cpp
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -1,122 +1,56 @@
 class Solution {
 public:
-    void merge(int low, int mid, int high,
-                vector <int> &positions,
-                vector <pair <int, int>> &robots)
-    {
-        vector <pair <int, int>> tmpRobot;
-        vector <int> tmpPos;
-
-        int left = low, right = mid + 1;
-        while(left <= mid && right <= high)
-        {
-            if(positions[left] <= positions[right])
-            {
-                tmpPos.push_back(positions[left]);
-                tmpRobot.push_back(robots[left++]);
-            }
-            else
-            {
-                tmpPos.push_back(positions[right]);
-                tmpRobot.push_back(robots[right++]);
-            }
-        }
-
-        while(left <= mid)
-        {
-            tmpPos.push_back(positions[left]);
-            tmpRobot.push_back(robots[left++]);
-        }
-
-        while(right <= high)
-        {
-            tmpPos.push_back(positions[right]);
-            tmpRobot.push_back(robots[right++]);
-        }
-
-        for(int i = low; i <= high; i++)
-        {
-            positions[i] = tmpPos[i-low];
-            robots[i] = tmpRobot[i-low];
-        }
-    }
-
-    void mergeSort(int low, int high, 
-                   vector <int> &positions,
-                   vector <pair <int, int>> &robots)
-    {
-        if(low >= high)
-            return;
-
-        int mid = low + ((high - low) >> 1);
-        mergeSort(low, mid, positions, robots);
-        mergeSort(mid+1, high, positions, robots);
-
-        merge(low, mid, high, positions, robots);
-    }
-
     vector <int> survivedRobotsHealths(vector <int> &positions, 
                                        vector <int> &healths, 
-                                       string &directions) 
+                                       string directions) 
     {
         int n = positions.size();
 
+        vector <int> indx(n);
+        iota(indx.begin(), indx.end(), 0);
 
-        vector <pair <int, int>> robots(n);
-        for(int i = 0; i < n; i++)
-            robots[i] = make_pair(healths[i] * ((directions[i] == 'R') - (directions[i] == 'L')), i);
-        mergeSort(0, n-1, positions, robots);
-
-        vector <pair <int, int>> tmp;
-        int i = 0;
-        while(i < n && robots[i].first < 0)
+        sort(indx.begin(), indx.end(), [&positions](int a, int b)
         {
-            tmp.push_back(make_pair(-robots[i].first, robots[i].second));
-            i++;
-        }
-
-        stack <pair <int, int>> stk;
-        while(i < n)
-        {
-            auto [hp, originalPos] = robots[i];
-
-            if(hp > 0)
-                stk.push(robots[i]);
-            else // left going robot collides with right going robots in the stack.
-            {
-                hp = -hp;
-                // collide as long as left robot has higher health.
-                while(!stk.empty() && hp > stk.top().first)
-                {
-                    hp--;
-                    stk.pop();
-                }
-
-                if(stk.empty()) // current robot killed all robots in the stack.
-                    tmp.push_back(make_pair(hp, originalPos));
-                else if(hp == stk.top().first) // both robots kill each other.
-                    stk.pop();
-                else // robot on top of stack killed current robot.
-                    stk.top().first--;
-            }
-
-            i++;
-        }
-
-        while(!stk.empty())
-        {
-            tmp.push_back(stk.top());
-            stk.pop();
-        }
-
-        sort(tmp.begin(), tmp.end(), [](pair <int, int> &a, pair <int, int> &b)
-        {
-            return a.second < b.second;
+            return positions[a] < positions[b];
         });
 
+        vector <int> stk;
+        for(int i = 0; i < n; i++)
+        {
+            int curr = indx[i];
+            if(directions[curr] == 'R')
+                stk.push_back(curr);
+            else
+            {
+                while(!stk.empty() && healths[curr] > 0)
+                {
+                    int top = stk.back();
+                    if(healths[curr] > healths[top])
+                    {
+                        healths[top] = -1;
+                        healths[curr]--;
+                        stk.pop_back();
+                    }
+                    else if(healths[curr] < healths[top])
+                    {
+                        healths[top]--;
+                        healths[curr] = -1;
+                    }
+                    else
+                    {
+                        healths[curr] = -1;
+                        healths[top] = -1;
+                        stk.pop_back();
+                    }
+                }
+
+            }
+        }
+
         vector <int> ans;
-        for(auto [hp, pos] : tmp)
-            ans.push_back(hp);
+        for(int i : healths)
+            if(i != -1)
+                ans.push_back(i);
 
         return ans;
     }
