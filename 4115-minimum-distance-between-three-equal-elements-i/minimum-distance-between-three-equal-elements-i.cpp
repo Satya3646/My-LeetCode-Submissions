cd6209d9293e2f2edd2c8ff9697cd563274120d8
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-04-10 13:41:37 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (49.58%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
@@ -3,16 +3,19 @@ public:
     int minimumDistance(vector <int> &nums)
     {
         int n = nums.size();
+
+        // Get the sorted order indices of nums.
         vector <int> indx(n);
         iota(indx.begin(), indx.end(), 0);
         sort(indx.begin(), indx.end(), [&nums](int a, int b)
         {
-            if(nums[a] == nums[b])
+            if(nums[a] == nums[b]) // for same values indices will be in sorted order.
                 return a < b;
             return nums[a] < nums[b];
         });
 
         int minDist = INT_MAX;
+        // Consecutive occourences will result in smallest distances.
         for(int x = 2; x < n; x++)
         {
             int i = indx[x-2];
