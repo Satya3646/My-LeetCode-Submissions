968a0bd8d26e2eaf798f1410905ac0e0854c9129
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-04-11 08:55:03 +0530

    Time: 579 ms (37.90%) | Memory: 276 MB (27.39%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -6,36 +6,47 @@ public:
         int n = coins[0].size();
 
         vector <vector <vector <int>>> dp(m, vector <vector <int>> (n, vector <int> (3, 0)));
-        dp[0][0][0] = coins[0][0]; 
+        dp[0][0][0] = coins[0][0];
         dp[0][0][1] = max(0, coins[0][0]);
         dp[0][0][2] = max(0, coins[0][0]);
 
-        for(int i = 0; i < m; i++)
+        // current cell (0, j) can only be reached from (0, j-1).
+        for(int j = 1; j < n; j++)
         {
-            for(int j = 0; j < n; j++)
+            for(int k = 0; k < 3; k++)
             {
-                if(i == 0 && j == 0)
-                    continue;
-                
+                int val = dp[0][j-1][k] + coins[0][j];
+                if(coins[0][j] < 0 && k > 0)
+                    val = max(val, dp[0][j-1][k-1]);
+                dp[0][j][k] = val;
+            }
+        }
+
+        // current cell (i, 0) can only be reached from (i-1, 0).
+        for(int i = 1; i < m; i++)
+        {
+            for(int k = 0; k < 3; k++)
+            {
+                int val = dp[i-1][0][k] + coins[i][0];
+                if(coins[i][0] < 0 && k > 0)
+                    val = max(val, dp[i-1][0][k-1]);
+                dp[i][0][k] = val;
+            }
+        }
+
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
                 for(int k = 0; k < 3; k++)
                 {
-                    int val = -1e8;
-                    if(i > 0)
-                    {
-                        val = max(val, dp[i-1][j][k] + coins[i][j]); // no neutralization.
-                        if(coins[i][j] < 0 && k > 0) // neutralization.
-                            val = max(val, dp[i-1][j][k-1]);
-                    }
-                    if(j > 0)
-                    {
-                        val = max(val, dp[i][j-1][k] + coins[i][j]); // no neutralization.
-                        if(coins[i][j] < 0 && k > 0) // neutralization.
-                            val = max(val, dp[i][j-1][k-1]);
-                    }
-                    dp[i][j][k] = val;
+                    dp[i][j][k] = max(dp[i-1][j][k], dp[i][j-1][k]) + coins[i][j];
+                    if(coins[i][j] < 0 && k > 0)
+                        dp[i][j][k] = max({dp[i][j][k], dp[i-1][j][k-1], dp[i][j-1][k-1]});
                 }
             }
         }
+
         return dp[m-1][n-1][2];
     }
 };
